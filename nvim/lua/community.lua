8e324eb5d18e79ede1d9f1f1392db2fcb93725de
cncsl <justitacsl@outlook.com>
2025-04-09 10:24:16 +0800

    feat(nvim): astronvim v5 don't need custom lsp-file-operations

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -13,8 +13,6 @@ return {
   --ui/ux
   { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
-  -- lsp
-  { import = "astrocommunity.lsp.nvim-lsp-file-operations"},
   -- 暂时没有自定义需求的 pack
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
